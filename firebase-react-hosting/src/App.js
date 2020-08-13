b8de7db90f6cb72f3c8f53cb1f0e6d80fbfebf3a
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-08-13 20:35:14 +0900

    complete post & list

--- firebase-react-hosting/src/App.js
+++ firebase-react-hosting/src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useReducer } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Form, InputGroup, Input, InputGroupAddon, Button, Table } from 'reactstrap';
 
 function App() {
+  const [value, setValue] = useState('')
+  const [todos, setTodos] = useState([])
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    addTodo(value)
+    setValue('')
+  }
+
+  const addTodo = text => {
+    const newTodos = [...todos, text]
+    setTodos(newTodos)
+    console.log(newTodos)
+  }
+
+  const removeTodo = index => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1 className="mt-4">Todo リスト</h1>
+        <Form onSubmit={handleSubmit}>
+          <InputGroup>
+            <Input type="text"
+              value={value}
+              onChange={e => setValue(e.target.value)} />
+            <InputGroupAddon addonType="append">
+              <Button type="submit" color="primary">追加</Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </Form>
+      </Container>
+
+      {/* リスト表示 */}
+      <Container>
+        <Table>
+          <tbody>
+            {todos && todos.map((todo, index) => (
+            <tr key={index}>
+              <th className="text-left">
+                {todo}
+              </th>
+              <td className="text-right">
+                <Button color="danger" onClick={() => removeTodo(index)}>削除</Button>
+              </td>
+            </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
     </div>
   );
 }
